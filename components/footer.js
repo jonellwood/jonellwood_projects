7e59397d8698c4547fb3a81a70323cc487a32dc2
Jon Ellwood <jonellwood@gmail.com>
2022-07-06 10:12:04 -0400

    made some trees with different shades

--- components/footer.js
+++ components/footer.js
@@ -1,19 +1,18 @@
 import Image from "next/image";
 import tree from "../public/images/tree.svg";
+import tree2 from "../public/images/tree2.svg";
 import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 
 export default function () {
   return (
     <footer className={styles.footer}>
-      <Image
-        src={tree}
-        alt="Tree"
-        priority
-        layout="fixed"
-        className={utilStyles.tree}
-        height={150}
-      />
+      <div className={utilStyles.tree}>
+        <Image src={tree} alt="Tree" priority layout="fixed" height={150} />
+      </div>
+      <div className={utilStyles.tree2}>
+        <Image src={tree2} alt="Tree" priority layout="fixed" height={185} />
+      </div>
       <p>
         Site made with <a href="https://nextjs.org/">NextJS</a>
       </p>
